21985f280963f5029b5658c88c273ce628336c79
Justin <justinshox0907@gmail.com>
2024-03-15 01:19:33 +0900

    Initial Commit

---
+++ JustinCard/JustinCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustinCardApp.swift
+//  JustinCard
+//
+//  Created by Justin on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct JustinCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
